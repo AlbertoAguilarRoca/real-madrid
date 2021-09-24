95e7418bf39a837671ef8357f379883d5ba9e82c
Alberto Aguilar <75422277+AlbertoAguilarRoca@users.noreply.github.com>
2021-09-24 08:53:11 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="ES">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Real Madrid CF</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        <!-- CSS bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <!-- ALGUNOS ESTILOS BASE -->
+        <link rel="stylesheet" href="css/main.css">
+        <!-- NUESTRO CSS -->
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+    <!-- CONDICIONAL PARA IE9 E INFERIORES -->
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">Estás usando un navegador <strong>obsoleto</strong>. Por favor <a href="http://browsehappy.com/">actualiza tu navegador</a>
+             para mejorar la experiencia de navegación.</p>
+        <![endif]-->
+
+        <!-- Header -->
+        <div class="container">
+        <header>
+            <div class="cabecera">
+                <div class="row">
+                    <div class="col">
+                        <div class="logo">
+                            <img src="img/real-madrid-c-f.svg">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <p class="logotipo">Real<span>madrid</span></p>
+                    </div>
+                </div><!--end row-->
+                <div class="menu-burguer">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="menu">
+                    <nav>
+                        <ul class="menu-nav">
+                            <li class="menu-nav-element"><a href="#">Inicio</a></li>
+                            <li class="menu-nav-element"><a href="#">Futbol</a></li>
+                            <li class="menu-nav-element"><a href="#">Baloncesto</a></li>
+                            <li class="menu-nav-element"><a href="#">El club</a></li>
+                            <li class="menu-nav-element"><a href="#">Aficionados</a></li>
+                            <li class="menu-nav-element"><a href="#">Estadio</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div><!--final div cabecera-->
+        </header>
+        </div><!-- end div container -->
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php';?>
+
+<!-- Aquí va el cuerpo de la web, después del <header> -->
+
+
+<!-- End de la web -->
+
+<?php include 'footer.php';?>
