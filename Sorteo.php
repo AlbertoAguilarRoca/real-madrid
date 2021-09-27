6d1eaaea09c64b44895b77badb68af21ffcb153f
Juan Villegas Muñoz <juan.villegasm21@gmail.com>
2021-09-27 10:32:06 +0200

    sorteo js hecho

--- Sorteo.php
+++ Sorteo.php
@@ -1,13 +1,20 @@
 <?php include 'header.php';?>
 
 <!-- Aquí va el cuerpo de la web, después del <header> -->
+<script src="js/sorteo.js"></script>
+
  
 <h1>Sorteo camiseta.</h1>
 
 <p >Entra en el sorteo de una camiseta de tu equipo favorito firmada por todos los jugadores.</p>
 
+<button onclick="sorteo()"> Sorteo </button>
+
+
 
 <div class="camiseta"><img src="img/camiseta.jpg"></div>
+
+
 
 
 
---
+++ js/sorteo.js
@@ -0,0 +1,16 @@
+
+function sorteo(){
+var numeroGanador=5;
+
+var numero=prompt("Introduzca un numero");
+
+
+if (numeroGanador==numero) {
+    alert("Has ganado el sorteo!!!");
+    
+} else {
+
+    alert("Lo sentimos, esperamos que tengas mas suerte la proxima vez");
+
+}
+}
