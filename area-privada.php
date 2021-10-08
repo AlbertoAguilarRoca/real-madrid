d07411a37999f47cfe0666dbc9db5b931fd1cbfb
Alberto Aguilar Roca <alberto.ma34026@cesurformacion.com>
2021-10-08 10:31:49 +0200

    Lista de Jugadores

--- area-privada.php
+++ area-privada.php
@@ -8,9 +8,22 @@
         <h1 class="titulo-login">Historia que tu has hecho</h1>
     </div>
 
+    <!--Lista De Jugadores-->
+    <div class="jugadores">
+        <h2>Mi lista de jugadores favoritos</h2>    
+        <p>Escribe el nombre de los jugadores, uno por uno, que quieras añadir a tu lista.</p>
+        <input id="jugadorForm" type="text" placeholder="Marcos Asensio" class="inputjugador">
+        <button class="delete-boton" onclick="borrarJugador()">Eliminar</button>
+        <button class="add-boton" onclick="addJugador()">Añadir</button> 
+        <h3 class="subtitulo-lista">Mi lista:</h3>
+        <p id="jugadoresFav"></p>
+    </div>
+
 </div> <!-- fin de container -->
 
 
 <!-- End de la web -->
+
+<script src="js/lista-jugadores.js"></script>
 
 <?php include 'footer.php';?>
