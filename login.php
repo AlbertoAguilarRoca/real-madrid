2ef044bcfebbff9967f12d61c9b91db792b61f60
Juanillo21 <juan.ma44602@cesurformacion.com>
2021-10-22 12:36:15 +0200

    story 2 casi terminado

--- login.php
+++ login.php
@@ -15,9 +15,10 @@
             <input id="nameUser" type="text" placeholder="Usuario" name="user">
             <input id="passwordUser" type="password" placeholder="Contraseña" name="password">
             <input type="submit" value="Iniciar sesión" onclick="login()">
+            <p><input type="checkbox" id="caja">Para recordarme en este equipo.</p>
 
             <p id="mensajeLogin" class="resultado-val"></p>
-            
+            <p id="mensajevacio" ></p>
             <p class="registro"><a href="#">¿No tienes cuenta? Regístrate</a></p>
         </div>
     </div>
