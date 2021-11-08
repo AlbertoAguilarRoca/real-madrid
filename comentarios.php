342fdb7569c49fce1b749d6a79f51ed7aa9c6db2
luisaranda33 <luisluisaranda@gmail.com>
2021-11-08 10:48:22 +0100

    Linkear css y crear página comentarios

---
+++ comentarios.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ES">
+    <head>
+
+        <!-- ALGUNOS ESTILOS BASE -->
+    <link rel="stylesheet" href="css/main.css">
+
+    </head>
+
+    <body>
+
+
+    
+    </body>
