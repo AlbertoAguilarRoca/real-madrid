e642ababc42f9c15234cfbaf1368972f13585245
Juan <juan.villegasm21@gmail.com>
2021-12-05 16:19:40 +0100

    comentarios arreglados

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,3 +1,4 @@
+var contenido = document.querySelector('#contenido');
 getUsers().then(arrayUsers => {
     imprimirUsuarios(arrayUsers);
     getPost().then(arrayPost => {
@@ -30,15 +31,14 @@ async function getUsers() {
 }
 //imprimimos los usuarios con su id y nombre
 function imprimirUsuarios(arrayUsers) {
+    console.log(arrayUsers);
 
     //con este for recorremos el array imprimiendo el id de usuario y nombre
     for (i = 0; i < arrayUsers.length; i++) {
-        var parrafo1 = document.createElement("P");
-        parrafo1.innerHTML = "El id del usuario es :" + arrayUsers[i].id;
-
-        var parrafo2 = document.createElement("P");
-        parrafo2.innerHTML = "Su nombre es :" + arrayUsers[i].name;
-
+        contenido.innerHTML += `
+       <p>El id del usuario es :  ${arrayUsers[i].id}</p>
+       <p>Su nombre es : ${arrayUsers[i].name}</p>
+        `
     }
 
 }
@@ -68,7 +68,7 @@ async function getPost() {
 //con esta funcion recorremos ambos arrays devueltos por el fechts
 //para imprimir los usuarios su id sus comentarios y compararlos en los diferentes arrays que toma como parametros.
 function imprimirFinal(arrayPost, arrayUsers) {
-    var contenido = document.getElementById("contenido");
+
 
 
     //variable para contabilizar los comentarios y su contenido
@@ -77,41 +77,25 @@ function imprimirFinal(arrayPost, arrayUsers) {
     //con este 1er for recorremos los la api de usuarios imprimiendo el usuario id y el nombre
     for (i = 0; i < arrayUsers.length; i++) {
 
-        var conParra1 = document.createElement("P");
-        conParra1.id = "parrafo1";
-        var conParra2 = document.createElement("P");
-        conParra2.id = "parrafo2";
-
-        conParra1.innerHTML = "El id de usuario es :" + arrayUsers[i].id;
-        conParra2.innerHTML = "El usuario es :" + arrayUsers[i].name;
-
-        var parrafo1 = document.getElementById("parrafo1");
-        var parrafo2 = document.getElementById("parrafo2");
-
-        contenido.appendChild(parrafo1);
-        contenido.appendChild(parrafo2);
-        // este segundo for recorre el post 
+        contenido.innerHTML += `
+        <hr>
+         <p>El id de usuario es :  ${arrayUsers[i].id}</p> 
+         <p>El usuario es :  ${arrayUsers[i].name}</p>
+         `
+            // este segundo for recorre el post 
         for (x = 0; x < arrayPost.length; x++) {
             //comprobando si el comentario coincide con el id del usuario 
             //en caso de ser asi imprimimos todos sus titulos y comentarios
             if (arrayPost[x].userId == arrayUsers[i].id) {
 
-                var conParra3 = document.createElement("P");
-                conParra3.id = "parrafo3";
-                var conParra4 = document.createElement("P");
-                conParra4.id = "parrafo4";
+                contenido.innerHTML += `
+                <p>Su comentario ${y} es : ${arrayPost[x].title}</p>
 
-                conParra3.innerHTML = "Su comentario " + y + " es :" + arrayPost[x].title;
+                <p>El contenido del comentario ${y}  es :  ${arrayPost[x].body}</p>
 
-                conParra4.innerHTML = "El contenido del comentario " + y + " es :" + arrayPost[x].body;
-
-                var parrafo3 = document.getElementById("parrafo3");
-                var parrafo4 = document.getElementById("parrafo4");
-
-                contenido.appendChild(parrafo3);
-                contenido.appendChild(parrafo4);
-
-                //aumentamos la variable por cada nuevo comentario
+              
+                `
+                    //aumentamos la variable por cada nuevo comentario
                 y++;
             }
 
